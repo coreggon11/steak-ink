fb647c7793d68f2fc5f3a9790ae32dbc56659121
Dominik | Supercolony <43150707+coreggon11@users.noreply.github.com>
2023-01-31 18:31:58 +0100

    voting contract is compiling

--- contracts/voting/lib.rs
+++ contracts/voting/lib.rs
@@ -4,29 +4,26 @@
 #[openbrush::contract]
 mod voting {
     use openbrush::traits::Storage;
-    // use steak_ink::impls::voting::{
-    //     Data as VotingData,
-    //     *,
-    // };
+    use steak_ink::impls::voting::{
+        Data as VotingData,
+        *,
+    };
 
     #[ink(storage)]
     #[derive(Default, Storage)]
     pub struct VotingContract {
-        // #[storage_field]
-        // voting: VotingData,
+        #[storage_field]
+        voting: VotingData,
     }
 
-    // impl Voting for VotingContract {}
+    impl Voting for VotingContract {}
 
     impl VotingContract {
         #[ink(constructor)]
         pub fn new(steakoin_account: AccountId) -> Self {
             let mut instance = Self::default();
-            // instance.voting.steakoin = steakoin_account;
+            instance.voting.steakoin = steakoin_account;
             instance
         }
-
-        #[ink(message)]
-        pub fn message(&self) {}
     }
 }
--- src/impls/mod.rs
+++ src/impls/mod.rs
@@ -1,5 +1,10 @@
+#[cfg(feature = "steakoin")]
 pub mod steakoin;
+#[cfg(feature = "voting")]
 pub mod voting;
 
+#[cfg(feature = "steakoin")]
 pub use steakoin::*;
+
+#[cfg(feature = "voting")]
 pub use voting::*;
