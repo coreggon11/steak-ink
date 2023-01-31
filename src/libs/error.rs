3d9c1371dff9cf15a0fc18ba6157182ee73733dc
Dominik | Supercolony <43150707+coreggon11@users.noreply.github.com>
2023-01-31 17:27:17 +0100

    create error

---
+++ src/libs/error.rs
@@ -0,0 +1,29 @@
+use openbrush::{
+    contracts::psp22::PSP22Error,
+    traits::String,
+};
+use scale::{
+    Decode,
+    Encode,
+};
+
+#[derive(Debug, PartialEq, Eq, Encode, Decode)]
+#[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+pub enum SteakErr {
+    PSP22Error(PSP22Error)
+}
+
+impl From<SteakErr> for PSP22Error {
+    fn from(err: SteakErr) -> Self {
+        match err {
+            SteakErr::PSP22Error(err) => err,
+            _ => PSP22Error::Custom(String::from("Custom")),
+        }
+    }
+}
+
+impl From<PSP22Error> for SteakErr {
+    fn from(err: PSP22Error) -> Self {
+        SteakErr::PSP22Error(err)
+    }
+}
